02709f2683412e18b4899c8bd2b56005cbab444a
mugnn <lgcgusmao@gmail.com>
2024-05-13 21:06:47 -0300

    add files

---
+++ src/main/java/com/expresstificate/ExpresstificateApplication.java
@@ -0,0 +1,13 @@
+package com.expresstificate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpresstificateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpresstificateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/expresstificate/ExpresstificateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.expresstificate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpresstificateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
